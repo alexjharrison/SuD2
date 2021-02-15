58a105f9e096c664df019dc56791c51cc66da73b
Alex Harrison <alexjharrison@gmail.com>
2021-02-14 22:13:05 -0500

    create wrapper for client socket events

--- client/src/hooks/useSocket/index.ts
+++ client/src/hooks/useSocket/index.ts
@@ -1,8 +1,28 @@
 import * as socketEvents from "./socketEvents";
-import { io } from "socket.io-client";
+import { io, Socket } from "socket.io-client";
 import { socketListeners } from "./socketListeners";
+import Emitter from "component-emitter";
+import { SocketActionTitles } from "common/socketEvents";
 
 export const socket = io();
+
+interface EmitterCallback<T> {
+  (data: T): void;
+}
+
+interface WrappedSocketClient<T> {
+  emit: (data: T) => Socket;
+  on: (callback: EmitterCallback<T>) => Emitter<SocketActionTitles>;
+}
+
+export function createSocket<T>(
+  event: SocketActionTitles
+): WrappedSocketClient<T> {
+  return {
+    emit: data => socket.emit(event, data),
+    on: callback => socket.on(event, callback)
+  };
+}
 
 // enable socket listeners
 socketListeners(socket);
